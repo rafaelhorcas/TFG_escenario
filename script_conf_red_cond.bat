e3313a38e81c7ed6b9856eadf37a3b44b39884a0
rafaelhorcas <rafaelhorcasm@gmail.com>
2024-05-22 12:20:16 +0200

    v6 scenario

--- script_conf_red_cond.bat
+++ script_conf_red_cond.bat
@@ -1,4 +1,6 @@
 @echo off
-netsh interface ipv4 add address "rem-switch" 10.0.3.2 255.255.255.0 gateway="10.0.3.1"
-netsh interface ipv4 add address "qos-switch" 10.0.0.16 255.255.255.0 gateway="10.0.2.12"
+netsh interface ipv4 add address "Ethernet 2" 10.0.3.2 255.255.255.0 gateway="10.0.3.1"
+route -p add 10.0.1.0 MASK 255.255.255.0 10.0.3.1
+netsh interface ipv4 add address "Ethernet 3" 10.0.0.16 255.255.255.0 
+route -p add 10.0.0.0 MASK 255.255.255.0 10.0.0.13
 echo Configuracion de red completada
--- script_conf_red_veh.bat
+++ script_conf_red_veh.bat
@@ -1,3 +1,4 @@
 @echo off
-netsh interface ipv4 add address "veh-switch" 10.0.2.2 255.255.255.0 gateway="10.0.2.1"
+netsh interface ipv4 add address "Ethernet 2" 10.0.1.2 255.255.255.0 gateway="10.0.1.1"
+route -p add 10.0.3.0 MASK 255.255.255.0 10.0.1.1
 echo Configuracion de red completada
